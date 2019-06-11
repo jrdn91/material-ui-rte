b6b19d267503c16e74de8ac7477dc6ce9acdeb0c
Jordan Riser <riser.jordan@gmail.com>
2019-06-11 16:01:05 -0400

    add default theme with theme provider for dev

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react"
 import { withStyles } from "@material-ui/styles"
 import Editor from "../src/Editor"
+import { ThemeProvider } from '@material-ui/styles';
+import theme from "./themes/main"
 
 const styles = {
   editor: {
@@ -13,9 +15,11 @@ class App extends Component {
   render() {
     const { classes } = this.props
     return (
-      <div className={classes.editor}>
-        <Editor />
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className={classes.editor}>
+          <Editor />
+        </div>
+      </ThemeProvider>
     )
   }
 }
