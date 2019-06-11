21160f0a721c9dfd946f3386d49943a225d1cbb9
Jordan Riser <riser.jordan@gmail.com>
2019-06-11 17:03:12 -0400

    update styles for toolbar

--- src/Editor/styles.js
+++ src/Editor/styles.js
@@ -1,6 +1,8 @@
 const styles = theme => ({
   toolbar: {
-    marginBottom: theme.spacing(2)
+    marginBottom: theme.spacing(2),
+    display: "flex",
+    justifyContent: "space-between"
   },
   editor: {
     padding: theme.spacing(2)
