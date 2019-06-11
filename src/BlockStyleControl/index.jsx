9180e914cc2371e0a2e79f35e5719012967a2b80
Jordan Riser <riser.jordan@gmail.com>
2019-06-11 14:28:50 -0400

    remove import

--- src/BlockStyleControl/index.jsx
+++ src/BlockStyleControl/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
