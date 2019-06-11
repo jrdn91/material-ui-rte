339d909c5701d81a726671d1aa5bff232fc764d2
Jordan Riser <riser.jordan@gmail.com>
2019-06-11 14:07:45 -0400

    add block style control snapshot test

---
+++ src/BlockStyleControl/index.test.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { shallow } from "enzyme"
+import BlockStyleControl from "./index"
+
+describe("BlockStyleControl", () => {
+  it("Should render block style control", () => {
+    const component = shallow(<BlockStyleControl />);
+  
+    expect(component).toMatchSnapshot();
+  });
+});
