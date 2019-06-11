b673056ab40799dab8635eb1e5406b9e5c30b0bb
Jordan Riser <riser.jordan@gmail.com>
2019-06-11 14:28:58 -0400

    wrap some styles around the dev page

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react"
+import { withStyles } from "@material-ui/styles"
 import Editor from "../src/Editor"
 
 const styles = {
+  editor: {
+    margin: "120px auto",
+    maxWidth: 768
+  }
 }
 
 class App extends Component {
   render() {
     const { classes } = this.props
     return (
-      <Editor />
+      <div className={classes.editor}>
+        <Editor />
+      </div>
     )
   }
 }
 
-export default App
+export default withStyles(styles, { withTheme: true })(App)
