d0d18145143dc6723f6e958a83ba4eb695b70d3a
Jordan Riser <riser.jordan@gmail.com>
2019-06-21 10:22:56 -0400

    add theme for testing

--- src/themes/main.js
+++ src/themes/main.js
@@ -1,3 +1,10 @@
 import { createMuiTheme } from "@material-ui/core/styles"
+import lime from "@material-ui/core/colors/lime"
 
-export default createMuiTheme({})
+export default createMuiTheme({
+  palette: {
+    text: {
+      primary: lime["A400"]
+    }
+  }
+})
