09600f4e463ef2039fc776e8b67eb0739b8ed25a
Jordan Riser <riser.jordan@gmail.com>
2019-06-22 16:23:24 -0400

    remove alignment controls prop on test app

--- src/app.js
+++ src/app.js
@@ -27,7 +27,6 @@ class App extends Component {
             blockStyleControls={["header-one"]}
             inlineStyleControls={["bold"]}
             listControls={["unordered-list"]}
-            alignmentControls={["center"]}
           />
         </div>
       </ThemeProvider>
