c912c721e7ae284aa6cd51bc3540efd6aabb1790
Jordan Riser <riser.jordan@gmail.com>
2019-06-26 15:05:15 -0400

    update story for editor with default theme

--- src/components/Editor/stories.js
+++ src/components/Editor/stories.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react"
+import { createMuiTheme } from "@material-ui/core/styles"
+import { ThemeProvider } from "@material-ui/styles"
 // Import the storybook libraries
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import { storiesOf } from "@storybook/react"
+import { action } from "@storybook/addon-actions"
 // Import our component from this folder
-import Editor from './index';
+import Editor from "./index"
 
-storiesOf('Editor')
-	.add('Basic', () => (
-		<Editor
-			onChange={state => action(state)}
-		/>
-	))
+const theme = createMuiTheme()
+
+storiesOf("Editor").add("Basic", () => (
+  <ThemeProvider theme={theme}>
+    <Editor onChange={action("editor content changed")} />
+  </ThemeProvider>
+))
