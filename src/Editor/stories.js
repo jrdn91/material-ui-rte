514d00ac507adf97eec14aa5c6fd65953a20adeb
Jordan Riser <riser.jordan@gmail.com>
2019-06-27 10:49:24 -0400

    add custom theme story

--- src/Editor/stories.js
+++ src/Editor/stories.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { createMuiTheme } from "@material-ui/core/styles"
 import { ThemeProvider } from "@material-ui/styles"
+import blueGrey from "@material-ui/core/colors/blueGrey"
+import lightBlue from "@material-ui/core/colors/lightBlue"
 // Import the storybook libraries
 import { storiesOf } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
@@ -10,7 +12,38 @@ import Editor from "./index"
 const theme = createMuiTheme({
   palette: {
     text: {
-      primary: "#f00"
+      primary: blueGrey[700]
+    }
+  },
+  overrides: {
+    MuiButton: {
+      contained: {
+        backgroundColor: blueGrey[50],
+        color: blueGrey[700]
+      }
+    },
+    MuiToolbar: {
+      regular: {
+        backgroundColor: [lightBlue[400], "!important"]
+      }
+    },
+    MuiInput: {
+      underline: {
+        "&:before": {
+          borderBottom: "1px solid white"
+        },
+        "&:after": {
+          borderBottom: "2px solid white"
+        },
+        "&:hover:not($disabled):before": {
+          borderBottom: "2px solid white"
+        }
+      }
+    },
+    MuiInputBase: {
+      root: {
+        color: "#fff"
+      }
     }
   }
 })
@@ -29,4 +62,9 @@ storiesOf("Editor")
       imageUploadControl={false}
       alignmentControls={["left", "center"]}
     />
+  ))
+  .add("Custom Theme", () => (
+    <ThemeProvider theme={theme}>
+      <Editor onChange={action("editor content changed")} />
+    </ThemeProvider>
   ))
