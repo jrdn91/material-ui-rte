2326613c81083170b603d308d023b3ae2b54d529
Jordan Riser <riser.jordan@gmail.com>
2019-07-01 13:17:19 -0400

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
-// Path is in Node for free and will make simple resolving of directories no
-// matter which part of your file system your library lives in
 const path = require("path")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const webpack = require("webpack")
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin
 
-// Webpack is just a bunch of keys on module.exports!
+/* Environment */
+const nodeEnv = process.env.NODE_ENV || "development"
+const DEVELOPMENT = nodeEnv === "development"
+const PRODUCTION = nodeEnv === "production"
+
 module.exports = {
-  // This is where our app starts. This is why we have done all this importing
-  // and exporting, to get to here
-  entry: "./src/index.js",
-  // module (I know it's a bit weird to have module.exports.module) is where we
-  // define all the rules for how webpack will deal with thing.
-  devtool: "source-map",
+  mode: nodeEnv,
+  entry: path.resolve(__dirname, "src", "index.js"),
   externals: [
     {
       react: "react",
@@ -19,76 +20,69 @@ module.exports = {
     },
     /^(\@material-ui\/[\w\/]+)$/i
   ],
-  module: {
-    // rules takes an array, each item containing the respective rules
-    rules: [
-      {
-        // First up, our JavaScript rules.
-        // If you want to use the .jsx extension, you can change this line to
-        // test: /\.jsx?$/,
-        // The ? in the regex just means "optional"
-        test: /\.jsx?$/,
-        // Don't bother spending time transpiling your installed packages
-        // exclude: /node_modules/,
-        // This is where we tell webpack to use babel to transpile our JS.
-        // The configuration can go here, but in this case it's in ./babelrc.js
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          { loader: "css-loader", options: { url: false, sourceMap: true } }
-        ]
-      },
-      {
-        // Some image formats so you can import images
-        test: /\.(png|gif|jpg|svg)$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 50000
-          }
-        }
-      }
-    ]
+  output: {
+    path: path.resolve(__dirname, "lib"),
+    filename: "index.js",
+    library: "Material-UI RTE",
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+    /**
+     * UMD modules refer to "window", which breaks SSR.
+     * @see https://github.com/webpack/webpack/issues/6522
+     */
+    globalObject: "typeof self !== 'undefined' ? self : this"
+  },
+  optimization: {
+    minimize: false
   },
   plugins: [
+    new webpack.DefinePlugin({
+      "process.env.BABEL_ENV": JSON.stringify(nodeEnv)
+    }),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       filename: "editor.css"
     })
-  ],
-  // Here we define explicitly the file types we intend to deal with
-  resolve: {
-    extensions: [
-      ".scss",
-      ".js",
-      ".jsx",
-      ".json",
-      ".png",
-      ".gif",
-      ".jpg",
-      ".svg"
+    // new BundleAnalyzerPlugin(),
+  ].filter(Boolean),
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: DEVELOPMENT
+            }
+          },
+          {
+            loader: "eslint-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: "../",
+              hmr: process.env.NODE_ENV === "development"
+            }
+          },
+          "css-loader"
+        ]
+      }
     ]
   },
-  // This is where we define how everything gets output.
-  // dist is a common output folder, and it should be gitignored. The build can
-  // be run after publishing so you don't wind up with it in source control
-  output: {
-    path: path.resolve(__dirname, "lib/"),
-    publicPath: "",
-    // You can do fun things here like use the [hash] keyword to generate unique
-    // filenames, but for this purpose rinse.js is fine. This file and path will
-    // be what you put in package.json's "main" field
-    filename: "index.js",
-    // This field determines how things are importable when installed from other
-    // sources. UMD may not be correct now and there is an open issue to fix this,
-    // but until then, more reading can be found here:
-    // https://webpack.js.org/configuration/output/#output-librarytarget
-    libraryTarget: "umd"
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx"]
   }
 }
